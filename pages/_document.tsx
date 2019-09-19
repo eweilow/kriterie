626396ee4771b802392dbf849fefa701c91a7269
E. Weilow <eweilow@digitalkoppar.se>
2019-09-19 16:38:52 +0200

    Set lang on document

---
+++ pages/_document.tsx
@@ -0,0 +1,17 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class KriterieDocument extends Document {
+  render() {
+    return (
+      <Html lang="sv">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default KriterieDocument;
