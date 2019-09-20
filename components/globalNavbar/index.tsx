0d0745830daeabfc2af418f933f49299e13a0467
E. Weilow <eweilow@digitalkoppar.se>
2019-09-20 10:19:40 +0200

    Add initial cookie page

--- components/globalNavbar/index.tsx
+++ components/globalNavbar/index.tsx
@@ -1,3 +1,17 @@
+import Link from "next/link";
+
 export const GlobalNavbar: React.FC = () => {
-  return <nav>nav nav nav</nav>;
+  return (
+    <>
+      <nav>
+        nav nav nav
+        <br />
+        <Link href="/">
+          <a>home</a>
+        </Link>
+      </nav>
+
+      <hr />
+    </>
+  );
 };
