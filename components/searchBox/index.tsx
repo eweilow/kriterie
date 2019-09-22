af3f0bbaaa85deb7d827ea32b48e709f15d0e63b
E. Weilow <eweilow@digitalkoppar.se>
2019-09-22 12:04:25 +0200

    More searchbox tweaks

--- components/searchBox/index.tsx
+++ components/searchBox/index.tsx
@@ -6,21 +6,20 @@ import {
   ComboboxPopover,
   ComboboxOptionText
 } from "@reach/combobox";
+import useResizeObserver from "use-resize-observer";
 
-import {
-  useEffect,
-  useCallback,
-  useState,
-  useRef,
-  useLayoutEffect
-} from "react";
+import { useEffect, useCallback, useState, useRef } from "react";
 
 import * as ExampleWorker from "./search.worker";
-import { Router, useRouter } from "next/router";
+import { useRouter } from "next/router";
 import clsx from "clsx";
 import { SearchLoadingBar } from "../loadingIndicator/searchBar";
 
-export const SearchBox: React.FC = () => {
+export const SearchBox: React.FC<{
+  zIndex?: number;
+  id: string;
+  initialSize?: number;
+}> = ({ id, initialSize = null, zIndex = 1 }) => {
   const [searchDisabled, setSearchDisabled] = useState(false);
   const [searchString, setSearchString] = useState("");
   const [searchResults, setSearchResults] = useState<any[]>(null);
@@ -60,51 +59,58 @@ export const SearchBox: React.FC = () => {
 
   const router = useRouter();
 
+  const mountedRef = useRef(false);
+  useEffect(() => {
+    mountedRef.current = true;
+    return () => {
+      mountedRef.current = false;
+    };
+  }, []);
+
   const handleSelect = useCallback(
     value => {
+      function start() {
+        setActive(true);
+        setLoading(true);
+        setSearchDisabled(true);
+      }
+      function waitForPromise(promise: Promise<any>) {
+        promise.then(() => {
+          if (mountedRef.current) {
+            setLoading(false);
+            setSearchString("");
+            setActive(false);
+            setSearchDisabled(false);
+          }
+        });
+      }
       setSearchString(value);
-      setActive(true);
-      setLoading(true);
+
       if (searchResults != null) {
         const found = searchResults.find(el => el.title === value);
         if (found.type === "program") {
           setTimeout(() => {
-            router
-              .push("/gy11/program/[id]", "/gy11/program/" + found.code)
-              .then(() => {
-                setLoading(false);
-                setSearchString("");
-                setActive(false);
-                setSearchDisabled(false);
-              });
+            waitForPromise(
+              router.push("/gy11/program/[id]", "/gy11/program/" + found.code)
+            );
           }, 500);
-          setSearchDisabled(true);
+          start();
         }
         if (found.type === "course") {
           setTimeout(() => {
-            router
-              .push("/gy11/course/[id]", "/gy11/course/" + found.code)
-              .then(() => {
-                setLoading(false);
-                setSearchString("");
-                setActive(false);
-                setSearchDisabled(false);
-              });
+            waitForPromise(
+              router.push("/gy11/course/[id]", "/gy11/course/" + found.code)
+            );
           }, 500);
-          setSearchDisabled(true);
+          start();
         }
         if (found.type === "subject") {
           setTimeout(() => {
-            router
-              .push("/gy11/subject/[id]", "/gy11/subject/" + found.code)
-              .then(() => {
-                setLoading(false);
-                setSearchString("");
-                setActive(false);
-                setSearchDisabled(false);
-              });
+            waitForPromise(
+              router.push("/gy11/subject/[id]", "/gy11/subject/" + found.code)
+            );
           }, 500);
-          setSearchDisabled(true);
+          start();
         }
       }
     },
@@ -120,14 +126,18 @@ export const SearchBox: React.FC = () => {
     setActive(false);
   }, []);
 
+  const [boxRef, boxWidth, boxHeight] = useResizeObserver({
+    defaultHeight: 40
+  });
+
   return (
     <Combobox
       openOnFocus
       className={clsx({ active, hasResults: searchResults })}
-      id="searchBox"
+      id={id}
       onSelect={handleSelect}
     >
-      <div className="box right">
+      <div data-size={boxHeight} ref={boxRef} className="box right">
         <div className="box left">
           <ComboboxInput
             onFocus={setFocused}
@@ -136,7 +146,6 @@ export const SearchBox: React.FC = () => {
             value={searchString}
             disabled={searchDisabled}
             aria-label="Innehållssök"
-            className="search-input"
             onChange={handleChange}
             placeholder="sök efter något"
           />
@@ -145,7 +154,7 @@ export const SearchBox: React.FC = () => {
         </div>
       </div>
       {searchResults && (
-        <ComboboxPopover portal={false} id="searchBoxPopover">
+        <ComboboxPopover portal={false} id={id + "Popover"}>
           <ComboboxList persistSelection aria-label="Innehållssök">
             {searchResults.map((result, index) => (
               <ComboboxOption key={index} value={`${result.title}`}>
@@ -166,21 +175,24 @@ export const SearchBox: React.FC = () => {
           box-sizing: border-box;
           color: #4d1a00;
           overflow: hidden;
-          z-index: 1;
           position: relative;
-          transform: translate3d(0, 0, 0);
+          z-index: 1;
         }
 
         .box.left {
           border-top-left-radius: 8px;
           border-bottom-left-radius: 8px;
           background: #eb7031;
-          transform: translate3d(calc(100% - 240px), 0, 0);
+          transform: ${
+            initialSize != null
+              ? `translate3d(calc(100% - ${initialSize}px), 0, 0)`
+              : "none"
+          };
           transition: transform 195ms cubic-bezier(0.4, 0, 0.2, 1);
         }
 
-        :global(#searchBox.active) .box.left,
-        :global(#searchBox[aria-expanded="true"].hasResults) .box.left {
+        :global(#${id}.active) .box.left,
+        :global(#${id}[aria-expanded="true"].hasResults) .box.left {
           transform: none;
         }
 
@@ -193,7 +205,7 @@ export const SearchBox: React.FC = () => {
           background: white;
           opacity: 0;
           transition: opacity 195ms cubic-bezier(0.4, 0, 0.2, 1);
-          transform: translate3d(0, 0, 0);
+          pointer-events: none;
         }
 
         :global(input:focus) + .background {
@@ -208,8 +220,8 @@ export const SearchBox: React.FC = () => {
           transition: filter 195ms cubic-bezier(0.4, 0, 0.2, 1);
         }
 
-        :global(#searchBox.active) .box.right,
-        :global(#searchBox[aria-expanded="true"].hasResults) .box.right {
+        :global(#${id}.active) .box.right,
+        :global(#${id}[aria-expanded="true"].hasResults) .box.right {
           filter: drop-shadow(0px 4px 8px #0000002a)
             drop-shadow(0px 2px 2px #0000002b);
         }
@@ -219,13 +231,14 @@ export const SearchBox: React.FC = () => {
           border-radius: 8px;
         }
 
-        :global(#searchBox[data-reach-combobox]) {
-          width: 320px;
+        :global(#${id}[data-reach-combobox]) {
+          z-index: ${zIndex};
           position: relative;
-          z-index: 1;
+          width: 100%;
+          height: 100%;
         }
 
-        :global(#searchBox [data-reach-combobox-input]),
+        :global(#${id} [data-reach-combobox-input]),
         .box {
           position: absolute;
           top: 0;
@@ -234,17 +247,16 @@ export const SearchBox: React.FC = () => {
           bottom: 0;
           width: 100%;
           box-sizing: border-box;
-          z-index: 1;
         }
 
-        :global(#searchBox [data-reach-combobox-input]) {
+        :global(#${id} [data-reach-combobox-input]) {
           background: none;
           border: none;
           padding: 4px 8px;
           outline: none !important;
         }
-        :global(#searchBox [data-reach-combobox-input]),
-        :global(#searchBox [data-reach-combobox-input])::placeholder {
+        :global(#${id} [data-reach-combobox-input]),
+        :global(#${id} [data-reach-combobox-input])::placeholder {
           color: #4d1a00;
           font-size: 18px;
           font-family: "GlacialIndifference", "Roboto", -apple-system,
@@ -252,7 +264,7 @@ export const SearchBox: React.FC = () => {
           font-weight: bold;
         }
 
-        :global(#searchBoxPopover[data-reach-combobox-popover]) {
+        :global(#${id + "Popover"}[data-reach-combobox-popover]) {
           position: absolute;
           top: -4px;
           left: -4px;
@@ -260,16 +272,17 @@ export const SearchBox: React.FC = () => {
           background: #ffb58f;
           color: #4d1a00;
           border-radius: 12px;
-          padding-top: 56px;
+          padding-top: ${boxHeight + 16}px;
           border: none;
           box-shadow: 0px 4px 24px #00000024, 0px 2px 4px #0000000f;
           padding-bottom: 8px;
 
           animation: enter 195ms cubic-bezier(0.4, 0, 0.2, 1) forwards;
+          outline: none;
         }
 
-        :global(#searchBox[aria-expanded="true"].hasResults)
-          :global(#searchBoxPopover[data-reach-combobox-popover]) {
+        :global(#${id}[aria-expanded="true"].hasResults)
+          :global(#${id + "Popover"}[data-reach-combobox-popover]) {
           animation-delay: 25ms;
           opacity: 0;
         }
@@ -283,39 +296,39 @@ export const SearchBox: React.FC = () => {
           }
         }
 
-        :global(#searchBoxPopover [data-reach-combobox-list]) {
+        :global(#${id + "Popover"} [data-reach-combobox-list]) {
           list-style: none;
           margin: 0;
           padding: 0;
           user-select: none;
         }
 
-        :global(#searchBoxPopover [data-reach-combobox-option]) {
+        :global(#${id + "Popover"} [data-reach-combobox-option]) {
           cursor: pointer;
           margin: 0;
           padding: 4px 8px;
           outline: none !important;
         }
 
-        :global(#searchBoxPopover
+        :global(#${id + "Popover"}
             [data-reach-combobox-option][aria-selected="true"]) {
-          background: hsl(211, 10%, 95%);
+          background: hsl(20, 100%, 70%);
         }
 
-        :global(#searchBoxPopover [data-reach-combobox-option]:hover) {
-          background: hsl(211, 10%, 92%);
+        :global(#${id + "Popover"} [data-reach-combobox-option]:hover) {
+          background: hsl(20, 100%, 68%);
         }
 
-        :global(#searchBoxPopover
+        :global(#${id + "Popover"}
             [data-reach-combobox-option][aria-selected="true"]:hover) {
-          background: hsl(211, 10%, 90%);
+          background: hsl(20, 100%, 66%);
         }
 
-        :global(#searchBoxPopover [data-user-value]) {
+        :global(#${id + "Popover"} [data-user-value]) {
           font-weight: bold;
         }
 
-        :global(#searchBoxPopover [data-suggested-value]) {
+        :global(#${id + "Popover"} [data-suggested-value]) {
           font-weight: normal;
         }
       `}</style>
