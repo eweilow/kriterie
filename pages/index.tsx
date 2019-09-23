cb79eab71e73626906321db15e34ee2015a6dba5
E. Weilow <eweilow@digitalkoppar.se>
2019-09-23 20:24:57 +0200

    Add a randomized data API endpoint for home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,11 @@
 import Link from "next/link";
 import { SearchBox } from "../components/searchBox";
+import { NextPage } from "next";
+import { fetchAndParseJson, wrappedInitialProps } from "../lib/notFound";
+import { getSafeUrl } from "../lib/safeUrl";
 
-export default () => (
+type Props = { data: any };
+const Page: NextPage<Props> = props => (
   <>
     <h1>Välkommen till kriterie.se!</h1>
     <p>
@@ -38,8 +42,29 @@ export default () => (
 
     <h2>Dagens slumpmässiga urval</h2>
     <h3>Kurser</h3>
+    {props.data.courses.map(el => (
+      <div key={el.code}>
+        <Link href="/gy11/course/[id]" as={`/gy11/course/${el.code}`}>
+          <a>{el.title}</a>
+        </Link>
+      </div>
+    ))}
     <h3>Ämnen</h3>
+    {props.data.subjects.map(el => (
+      <div key={el.code}>
+        <Link href="/gy11/subject/[id]" as={`/gy11/subject/${el.code}`}>
+          <a>{el.title}</a>
+        </Link>
+      </div>
+    ))}
     <h3>Program</h3>
+    {props.data.programmes.map(el => (
+      <div key={el.code}>
+        <Link href="/gy11/program/[id]" as={`/gy11/program/${el.code}`}>
+          <a>{el.title}</a>
+        </Link>
+      </div>
+    ))}
     <h2>Dina favoriter</h2>
     <p>
       Du har ännu inte lagt till något som dina favoriter. Detta gör du genom
@@ -47,3 +72,13 @@ export default () => (
     </p>
   </>
 );
+
+Page.getInitialProps = wrappedInitialProps<Props>(async ctx => {
+  const url = getSafeUrl(`/api/home`, ctx.req);
+  const data = await fetchAndParseJson(`Home data was not found`, url);
+  return {
+    data
+  };
+});
+
+export default Page;
