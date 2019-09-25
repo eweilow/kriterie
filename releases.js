0388593fd15b84ac5984297a8a3a4a522b3e37ec
E. Weilow <eweilow@digitalkoppar.se>
2019-09-25 21:08:35 +0200

    Initial releases setup

---
+++ releases.js
@@ -0,0 +1,51 @@
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { execSync } = require("child_process");
+
+const SENTRY_ORG = "personal-yl";
+const SENTRY_PROJECT = "kriterie";
+const SENTRY_LOG_LEVEL = "";
+
+const VERSION = execSync("yarn --silent sentry-cli releases propose-version", {
+  env: {
+    SENTRY_PROJECT,
+    SENTRY_ORG,
+    SENTRY_LOG_LEVEL
+  }
+})
+  .toString()
+  .trim();
+
+if (!/^[A-z0-9]+$/.test(VERSION)) {
+  console.log("Invalid version: '" + VERSION + "'");
+  return setTimeout(() => {
+    process.exit(1);
+  }, 500);
+}
+console.log("Using version: '" + VERSION + "'");
+
+console.log("Starting release");
+execSync(`yarn --silent sentry-cli releases new "${VERSION}"`, {
+  env: {
+    SENTRY_PROJECT,
+    SENTRY_ORG,
+    SENTRY_LOG_LEVEL
+  }
+});
+
+console.log("Assigning commits");
+execSync(`yarn --silent sentry-cli releases set-commits "${VERSION}" --auto`, {
+  env: {
+    SENTRY_PROJECT,
+    SENTRY_ORG,
+    SENTRY_LOG_LEVEL
+  }
+});
+
+execSync(`yarn --silent sentry-cli releases finalize "${VERSION}"`, {
+  env: {
+    SENTRY_PROJECT,
+    SENTRY_ORG,
+    SENTRY_LOG_LEVEL
+  }
+});
