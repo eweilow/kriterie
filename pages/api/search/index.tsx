4d29858da06a5e98e8c9c55a00113effd07537e0
E. Weilow <eweilow@digitalkoppar.se>
2019-09-25 19:57:45 +0200

    Force lint failure (test)

--- pages/api/search/index.tsx
+++ pages/api/search/index.tsx
@@ -7,23 +7,25 @@ export default (req: NextApiRequest, res: NextApiResponse) => {
     const programmes = loadProgrammes();
     const subjects = loadSubjects();
     const courses = loadCourses();
-    res.status(200).json([
-      ...programmes.map(el => ({
-        type: "program",
-        title: el.title,
-        code: el.code
-      })),
-      ...subjects.map(el => ({
-        type: "subject",
-        title: el.title,
-        code: el.code
-      })),
-      ...courses.map(el => ({
-        type: "course",
-        title: el.title,
-        code: el.code
-      }))
-    ]);
+    res
+      .status(200)
+      .json([
+        ...programmes.map(el => ({
+          type: "program",
+          title: el.title,
+          code: el.code
+        })),
+        ...subjects.map(el => ({
+          type: "subject",
+          title: el.title,
+          code: el.code
+        })),
+        ...courses.map(el => ({
+          type: "course",
+          title: el.title,
+          code: el.code
+        }))
+      ]);
   } catch (err) {
     catchError(err, req, res);
   }
