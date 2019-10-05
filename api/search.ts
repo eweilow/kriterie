6c105ac606272489a92c4070c3c1b2904863f44a
E. Weilow <eweilow@digitalkoppar.se>
2019-10-05 15:11:33 +0200

    Move search route to own function

---
+++ api/search.ts
@@ -0,0 +1,25 @@
+import { loadSubjects, loadProgrammes, loadCourses } from "./load";
+
+export function getSearchData() {
+  const programmes = loadProgrammes();
+  const subjects = loadSubjects();
+  const courses = loadCourses();
+
+  return [
+    ...programmes.map(el => ({
+      type: "program",
+      title: el.title,
+      code: el.code
+    })),
+    ...subjects.map(el => ({
+      type: "subject",
+      title: el.title,
+      code: el.code
+    })),
+    ...courses.map(el => ({
+      type: "course",
+      title: el.title,
+      code: el.code
+    }))
+  ];
+}
--- pages/api/search/index.tsx
+++ pages/api/search/index.tsx
@@ -1,29 +1,10 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { catchError } from "../../../api/helpers";
-import { loadSubjects, loadProgrammes, loadCourses } from "../../../api/load";
+import { getSearchData } from "../../../api/search";
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
   try {
-    const programmes = loadProgrammes();
-    const subjects = loadSubjects();
-    const courses = loadCourses();
-    res.status(200).json([
-      ...programmes.map(el => ({
-        type: "program",
-        title: el.title,
-        code: el.code
-      })),
-      ...subjects.map(el => ({
-        type: "subject",
-        title: el.title,
-        code: el.code
-      })),
-      ...courses.map(el => ({
-        type: "course",
-        title: el.title,
-        code: el.code
-      }))
-    ]);
+    res.status(200).json(getSearchData());
   } catch (err) {
     catchError(err, req, res);
   }
