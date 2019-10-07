3a0c95f92985c58426f599b0a0130a90168e05e9
E. Weilow <eweilow@digitalkoppar.se>
2019-10-07 10:00:01 +0200

    Better url generation

--- lib/safeUrl.ts
+++ lib/safeUrl.ts
@@ -1,14 +1,28 @@
 import { IncomingMessage } from "http";
 
+function getFirstNonNull(...values: string[]) {
+  for (const value of values) {
+    if (value != null) {
+      return value;
+    }
+  }
+  return null;
+}
+
 export function getSafeUrl(url: string, req?: IncomingMessage) {
+  const host = getFirstNonNull(
+    req.headers["now-deployment-url"] as string,
+    req.headers.host
+  );
+
   return `${
     req
       ? `${
-          req.headers.host.split(":")[0] === "localhost" ||
-          req.headers.host.split(":")[0].startsWith("192.168.")
+          host.split(":")[0] === "localhost" ||
+          host.split(":")[0].startsWith("192.168.")
             ? "http"
             : "https"
-        }://${req.headers.host}`
+        }://${host}`
       : ""
   }${url}`;
 }
