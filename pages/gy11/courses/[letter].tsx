8809c12d4539ae2d63cb756c467470abf0c975a1
E. Weilow <eweilow@digitalkoppar.se>
2020-01-14 10:58:21 +0100

    Refactor courses to getStaticProps

--- pages/gy11/courses/[letter].tsx
+++ pages/gy11/courses/[letter].tsx
@@ -1,44 +1,83 @@
 import { NextPage } from "next";
-import { wrappedInitialProps, fetchAndParseJson } from "../../../lib/notFound";
-import { getSafeUrl } from "../../../lib/safeUrl";
 import { NextSeo } from "next-seo";
 import { getAllCoursesData } from "../../../api/allCourses";
 import { CourseList } from "../../../components/courseList";
 import { LettersList } from "../../../components/lettersList";
+import { isNotFoundError } from "../../../api/helpers";
+import { loadCourses } from "../../../api/load";
+import KriterieError from "../../_error";
+
+export async function unstable_getStaticProps({ params }) {
+  try {
+    return {
+      props: {
+        data: await getAllCoursesData(params.letter.toLowerCase()),
+        letter: params.letter
+      },
+      revalidate: false
+    };
+  } catch (err) {
+    if (isNotFoundError(err)) {
+      return {
+        props: {
+          data: null
+        },
+        revalidate: false
+      };
+    }
+    throw err;
+  }
+}
+
+export async function unstable_getStaticPaths() {
+  const courses = loadCourses();
+
+  const letters = Array.from(
+    courses.reduce<Set<string>>(
+      (set, { title }) => set.add(title.toLowerCase()[0]),
+      new Set<string>()
+    )
+  );
+  letters.sort();
+
+  return letters.map(letter => ({
+    params: {
+      letter
+    }
+  }));
+}
 
 type Props = { data: ReturnType<typeof getAllCoursesData>; letter: string };
-const CoursesPage: NextPage<Props> = props => (
-  <>
-    <NextSeo
-      noindex={true}
-      canonical={`https://kriterie.se/gy11/courses/${props.letter.toLowerCase()}`}
-      title={`Kurser som börjar på '${props.letter}'`}
-    />
-    <LettersList
-      letters={props.data.letters}
-      activeLetter={props.letter}
-      formatAs={s => `/gy11/courses/${s.toLowerCase()}`}
-      formatHref={() => "/gy11/courses/[letter]"}
-    />
-    <h1>Kurser som börjar på {props.letter}</h1>
-    <CourseList
-      subjects={props.data.subjects.map(el => ({
-        ...el,
-        minPoints: null,
-        freeChoice: 0
-      }))}
-    />
-  </>
-);
+const CoursesPage: NextPage<Props> = props => {
+  if (props.data == null) {
+    return (
+      <KriterieError err={null} hasGetInitialPropsRun={true} statusCode={404} />
+    );
+  }
 
-CoursesPage.getInitialProps = wrappedInitialProps<Props>(async ctx => {
-  const letter = (ctx.query.letter as string).toLowerCase();
-  const url = getSafeUrl(`/api/courses/${encodeURIComponent(letter)}`, ctx.req);
-  const data = await fetchAndParseJson<any>("Courses not found", url);
-  return {
-    data,
-    letter
-  };
-});
+  return (
+    <>
+      <NextSeo
+        noindex={true}
+        canonical={`https://kriterie.se/gy11/courses/${props.letter.toLowerCase()}`}
+        title={`Kurser som börjar på '${props.letter}'`}
+      />
+      <LettersList
+        letters={props.data.letters}
+        activeLetter={props.letter}
+        formatAs={s => `/gy11/courses/${s.toLowerCase()}`}
+        formatHref={() => "/gy11/courses/[letter]"}
+      />
+      <h1>Kurser som börjar på {props.letter}</h1>
+      <CourseList
+        subjects={props.data.subjects.map(el => ({
+          ...el,
+          minPoints: null,
+          freeChoice: 0
+        }))}
+      />
+    </>
+  );
+};
 
 export default CoursesPage;
